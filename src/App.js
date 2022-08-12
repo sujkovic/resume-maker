a5e9d75d068e4c9727bd0e6c7cea41c5f935ca79
sujkovic <adrian.sujkovic@gmail.com>
2022-08-12 18:16:47 -0400

    safety commit before installing react-pdf

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Form from './components/Form';
+import './style.css';
 
 class App extends Component {
   constructor(props) {
@@ -8,7 +9,7 @@ class App extends Component {
 
   render() {
     return (
-      <div>
+      <div className='app'>
         <Form />
       </div>
     );
